4443a672ebdb40e3cd3e5b3bcabdf06c3e8c371d
Nikita Viira <nikita.viira@osom.finance>
2022-11-11 12:03:06 +0200

    test scheduler

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ scheduler.schedule("0 59 23 * * *", async function () {
   await axios.get(TELEGRAM_BOT_LINK + 'Я жив: ' + new Date().toISOString().slice(0, 10));
 });
 
-scheduler.schedule("0 0 12 * * *", async function () {
+scheduler.schedule("*/30 * * * * *", async function () {
   console.log("---------------------");
   console.log("Sending health check");
 
